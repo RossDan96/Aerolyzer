efe75e48d85b1122b332b3b1452a66015a1a38ae
aero <rossda@oregonstate.edu>
2018-02-07 18:02:52 -0800

    Finished test_location.py calls all location.py functions on prompted image when using test_location_all()

--- aerolyzer/location.py
+++ aerolyzer/location.py
@@ -4,80 +4,78 @@ import sys
 import os
 import wunderData
 
-'''
- Purpose:        The purpose of this script is to extract the Latitude and Longitude from the EXIF data
- Inputs:         tags: structure storing the image's EXIF data.
- Outputs:        coords: A tuple of the Latitude and Longitude in Decimal form
- Returns:        (lat,lon)
- Assumptions:    The EXIF data is valid.
- '''
-
-def get_coord(tags):
-    values = tags['gps gpslatitude'][1:-1].split(", ")
+def get_coord(exifdict):
+    '''
+    Purpose:        The purpose of this script is to extract the Latitude and Longitude from the EXIF data
+    Inputs:         exifdict: structure storing the image's EXIF data.
+    Outputs:        coords: A tuple of the Latitude and Longitude in Decimal form
+    Returns:        (lat,lon)
+    Assumptions:    The EXIF data is valid.
+    '''
+    values = exifdict['gps gpslatitude'][1:-1].split(", ")
     s = values[2]
     df = float(values[0])
     mf = float(values[1])
     smath = s.split("/")
     sf = float(smath[0])/float(smath[1])
     lat = df + mf/60 + sf/3600
-    if tags['gps gpslatituderef'] == 'S':
+    if exifdict['gps gpslatituderef'] == 'S':
         lat = lat*(-1)
 
-    values = tags['gps gpslongitude'][1:-1].split(", ")
+    values = exifdict['gps gpslongitude'][1:-1].split(", ")
     s = values[2]
     df = float(values[0])
     mf = float(values[1])
     smath = s.split("/")
     sf = float(smath[0])/float(smath[1])
     lon = df + mf/60 + sf/3600
-    if tags['gps gpslongituderef'] == 'W':
+    if exifdict['gps gpslongituderef'] == 'W':
         lon = lon*(-1)
 
     return (lat,lon)
 
 
-'''
- Purpose:        The purpose of this script is to convert Latitude and Longitude to a ZIP Code
- Inputs:         coord: tuple holding latitude and longitude
- Outputs:        ZIP: 5 digit long ZIP code.
- Returns:        
- Assumptions:    The EXIF data is valid.
- '''
 
-def coord_to_zip(coord):
+def coord_to_zip(coord,googlegeokey):
+    '''
+    Purpose:        The purpose of this script is to convert Latitude and Longitude to a ZIP Code
+    Inputs:         coord: tuple holding latitude and longitude, googlegeokey: The Google geocoding API
+    Outputs:        string of 5 digit long ZIP code.
+    Returns:        zipcode
+    Assumptions:    The EXIF data is valid.
+    '''
 
     try:
-#"+str(coord[0])+","+str(coord[1])+"
-        url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+str(coord[0])+","+str(coord[1])+"&key=AIzaSyD0SIrsNBNbE9-hSnfa6gMHALCdLZWJ6uI"
+        url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+str(coord[0])+","+str(coord[1])+"&key="+googlegeokey
         c = urllib2.urlopen(url)
         response = c.read()
         parsedResults = json.loads(response)
-        ZIP = parsedResults['results'][0]['address_components'][-1]['long_name']
+        zipcode = parsedResults['results'][0]['address_components'][-1]['long_name']
 
     except Exception:
         print "Unable to retrieve data: ", sys.exc_info()[0]
-        ZIP = "99999"
+        zipcode = "99999"
 
     finally:
-        return ZIP
+        return zipcode
 
-'''
- Purpose:        The purpose of this script is to convert ZIP Code to a Latitude and Longitude
- Inputs:         ZIP: 5 digit long ZIP code.
- Outputs:        coord: tuple holding latitude and longitude
- Returns:        
- Assumptions:    The EXIF data is valid.
- '''
 
-def zip_to_coord(zip):
 
+def zip_to_coord(zipcode,googlegeokey):
+    '''
+    Purpose:        The purpose of this script is to convert ZIP Code to a Latitude and Longitude
+    Inputs:         zipcode: 5 digit long ZIP code.
+    Outputs:        coord: tuple holding latitude and longitude
+    Returns:        (lat,lon)
+    Assumptions:    The EXIF data is valid.
+    '''
     try:
-        url = 'https://maps.googleapis.com/maps/api/geocode/json?address='+zip+'&key=AIzaSyD0SIrsNBNbE9-hSnfa6gMHALCdLZWJ6uI'
+        url = 'https://maps.googleapis.com/maps/api/geocode/json?address='+zipcode+'&key='+googlegeokey
         c = urllib2.urlopen(url)
         results = c.read()
         parsedResults = json.loads(results)
-	lat = parsedResults['results'][0]['geometry']['location']['lat']
-        lon = parsedResults['results'][0]['geometry']['location']['lng']
+	lat = float(parsedResults['results'][0]['geometry']['location']['lat'])
+        lon = float(parsedResults['results'][0]['geometry']['location']['lng'])
 
     except Exception:
         print "Unable to retrieve data: ", sys.exc_info()[0]
@@ -86,32 +84,28 @@ def zip_to_coord(zip):
     finally:
         return (lat,lon)
 
+
+def sun_position(exifdict):
     '''
-    Purpose:        The purpose of this function is to determine whether or not the I and
-                    II quadrants of the image have rgb values indicitive of a sky
-    Inputs:         list of lists red_sky, list of lists green_sky, list of lists blue_sky
-                    Note: Each inner list contains rgb for each pixel in a horizontal row
-    Outputs:        None
-    Returns:        0,1,2
+    Purpose:        Identify whether an image was taken during sunrise or sunset.
+    Inputs:         exifdict: structure storing the image's EXIF data.
+    Outputs:        string
+    Returns:        sunrise,sunset,night,day
     Assumptions:    N/A
     '''
-def sun_position(exifdict):
     coord = get_coord(exifdict)
-    data = wunderData.get_data(str(coord[0])+","+str(coord[1]))
-    sunriseTime = data['sunrise'].split(':')
-    sunsetTime = data['sunset'].split(':')
+    wData = wunderData.get_data(str(coord[0])+","+str(coord[1]))
+    sunriseTime = wData['sunrise'].split(':')
+    sunsetTime = wData['sunset'].split(':')
     sunriseTarget = (int(sunriseTime[0])*60)+int(sunriseTime[1])
     sunsetTarget = (int(sunsetTime[0])*60)+int(sunsetTime[1])
 
-    hoursTime = (exifdict['exif datetimeoriginal'].split(' '))[1].split(':')
+    hoursTime = (str(exifdict['exif datetimeoriginal']).split(' '))[1].split(':')
     pictureTime = (int(hoursTime[0])*60)+int(hoursTime[1])+int(float(hoursTime[2])/60)
 
-    #print sunriseTarget
-    #print sunsetTarget
-    #print pictureTime
     if ((pictureTime >= (sunriseTarget - 15)) & (pictureTime <= (sunriseTarget + 30))):
         return 'sunrise'
-    elif ((pictureTime >= (sunsetTarget - 30)) & (pictureTime <= (sunsetTarget + 15))):
+    elif ((pictureTime >= (sunsetTarget - 15)) & (pictureTime <= (sunsetTarget + 30))):
         return 'sunset'
     elif ((pictureTime > (sunsetTarget + 15))|(pictureTime < (sunriseTarget - 15))):
         return 'night'
--- aerolyzer/test.py
+++ aerolyzer/test.py
@@ -1,15 +1,4 @@
-from retrieve_image_data import RtrvData as Data
-import location
-from image_restriction_functions import imgRestFuncs as imgRestric
+import test_location
 
-mytest = Data("./")
-restrictest = imgRestric()
-exifdict = mytest.get_exif("/home/aero/Documents/Aerolyzer/Aerolyzer/aerolyzer/images/img3.JPG",True,True)
-print exifdict
-coord = location.get_coord(exifdict)
-print coord
-ZIP = location.coord_to_zip(coord)
-print ZIP
-coord2 = location.zip_to_coord(ZIP)
-print coord2
-print location.sun_position(exifdict)
+test_location.test_location_all()
+
