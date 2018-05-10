7e84bfa4ade194b19b8da314ba530fe042baf9ef
aero <rossda@oregonstate.edu>
2018-05-09 19:04:30 -0700

    Added docstrings and fixed whitespace

--- aerolyzer/test_aerosol.py
+++ aerolyzer/test_aerosol.py
@@ -1,63 +1,61 @@
-import cv2
+'Aerosol.py tests'
+import time
 import numpy as np
 import aerosol
-import time
-import math
-aero = aerosol.AeroData()
+aero = aerosol.AeroData("./images/img6.jpg")
 
 def test_all():
-        testspassed  = 0
-        numtests  = 0
-        if(test_analyzeWavelength()==1):
-                print "test_analyzeWavelength() passed\n"
-                testspassed+=1
-                numtests+=1
-        else:
-                numtests+=1
-                print "test_analyzeWavelength() failed\n"
-        if(test_readHazeLayer()==1):
-                print "test_readHazeLayer() passed\n"
-                testspassed+=1
-                numtests+=1
-        else:
-                numtests+=1
-                print "test_readHazeLayer() failed\n"
-
-
-
-        print "Number of Tests passed "+ str(testspassed) +"/"+str(numtests)+"\n"
-
+    '''
+    Purpose:        runs all test functions
+    Inputs:         None
+    Outputs:        Prints the number of tests that were passed
+    Returns:        None
+    Assumptions:    N/A
+    '''
+    testspassed = 0
+    numtests = 0
+    if test_analyzeWavelength() == 1:
+        print "test_analyzeWavelength() passed\n"
+        testspassed += 1
+        numtests += 1
+    else:
+        numtests += 1
+        print "test_analyzeWavelength() failed\n"
+    if test_aerolyzeImage() == 1:
+        print "test_aerolyzeImage() passed\n"
+        testspassed += 1
+        numtests += 1
+    else:
+        numtests += 1
+        print "test_aerolyzeImage() failed\n"
+        print "Number of Tests passed " + str(testspassed) + "/" + str(numtests) + "\n"
 
 def test_analyzeWavelength():
-        rand = np.random.random() *1000
-        wavelength = 300.0
-        print aero.analyzeWavelength(wavelength)
-        return 1
-def gen_hsvtestlist():
-        #randsize = int(((np.random.random() *10000)%200)**2)
-        randsize = 100
-        testList = []
-        print "Randsize: "+str(randsize)+"\n"
-        for i in range(randsize):
-                randh = (np.random.random() *1000)%180
-                rands = (np.random.random() *1000)%255
-                randv = (np.random.random() *1000)%255
-                testList.append([randh,rands,randv])
-        print "testlist done"
-        return testList,randsize
-def test_readHazeLayer():
-        t2 = time.time()
-        testList,numPixels = gen_hsvtestlist()
-        t3 = time.time()
-        timeRandoms = t3-t2
-        print "time gen_hsvtestlist(): "+ str(timeRandoms)
-        t0 = time.time()
-        print aero.readHazeLayer(testList)
-        t1 = time.time()
-        total_n = t1-t0
-        print "readHazeLayer runtime: "+ str(total_n)
-        speedpp = total_n/numPixels
-        print "readHazeLayer speed per pixel: "+ str(speedpp)
-        return 1
+    '''
+    Purpose:        Calls analyzeWavelength
+    Inputs:         None
+    Outputs:        Prints the result of analyzeWavelength
+    Returns:        1 if passed
+    Assumptions:    N/A
+    '''
+    rand = np.random.random() * 1000.0
+    wavelength = 300.0 + rand
+    print aero.analyzeWavelength(wavelength)
+    return 1
+
+def test_aerolyzeImage():
+    '''
+    Purpose:        Calls aerolyzeImage
+    Inputs:         None
+    Outputs:        Prints the result of aerolyzeImage, Prints the runtime of aerolyzeImage
+    Returns:        1 if passed
+    Assumptions:    N/A
+    '''
+    t0 = time.time()
+    print aero.aerolyzeImage()
+    t1 = time.time()
+    total_n = t1 - t0
+    print "readHazeLayer runtime: " + str(total_n)
+    return 1
 
 test_all()
